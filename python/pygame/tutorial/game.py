3d8150606a0e4fc3ff801053ea8174944ba1d8d9
Ruchit Patel <ruchittpatell@gmail.com>
2024-08-04 16:16:39 -0700

    animations

--- python/pygame/tutorial/game.py
+++ python/pygame/tutorial/game.py
@@ -4,8 +4,8 @@ Tutorial: https://www.youtube.com/watch?v=2gABYM5M0ww
 
 import pygame
 from sys import exit
-from scripts.entities import PhysicsEntity
-from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity, Player
+from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
 
@@ -29,12 +29,18 @@ class Game:
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
             'background': load_image('background.png'),
-            'clouds': load_images('clouds')
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide'), img_dur=6),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide'), img_dur=6),
+
         }
 
         self.clouds = Clouds(self.assets['clouds'], count=16)
 
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.player = Player(self, (50, 50), (8, 15))
         self.scroll = [0, 0]
 
         self.tilemap = Tilemap(self, tile_size=16)
--- python/pygame/tutorial/scripts/entities.py
+++ python/pygame/tutorial/scripts/entities.py
@@ -3,14 +3,24 @@ import pygame
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
         self.game = game
-        self.e_type = e_type
+        self.type = e_type
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
 
+        self.action = ''
+        self.anim_offset = (-3, -3)
+        self.flip = False
+        self.set_action('idle')
+
     def rectangle(self):
         return pygame.Rect(*self.pos, *self.size)
+
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
 
     def update(self, tilemap, movement=(0, 0)):
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
@@ -44,10 +54,37 @@ class PhysicsEntity:
                     entity_rect.top = rectangle.bottom
                 self.pos[1] = entity_rect.y
 
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
         self.velocity[1] = min(5, self.velocity[1]+0.1)         # fall gracefull (w/ terminal velocity)
 
         if self.collisions['up'] or self.collisions['down']:
             self.velocity[1] = 0
 
+        self.animation.update()
+
     def render(self, surface, offset):
-        surface.blit(self.game.assets[self.e_type], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+        surface.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisions['down']:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
